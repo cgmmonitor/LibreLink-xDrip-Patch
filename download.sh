6affa3a29e5d4da70ade95cc798f8bea503ff790
cgmmonitor <72459823+cgmmonitor@users.noreply.github.com>
2022-06-16 21:12:33 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -8,16 +8,22 @@ WHITE='\033[1;37m'
 YELLOW='\033[1;33m'
 
 WORKDIR=$(pwd)
-FILENAME='com.freestylelibre.app.de_2019-04-22'
+FILENAME_230='com.freestylelibre.app.de_2019-04-22'
+FILENAME_240='com.freestylelibre.app.de_2020-02-15'
 
 # wget HSTS-bugfix for debian as subsystem in Windows
 touch ~/.wget-hsts
 chmod 644 ~/.wget-hsts
 
-echo -e "${WHITE}Lade original APK herunter ...${NORMAL}"
-wget -O APK/apkpure.html --keep-session-cookies --save-cookies cookies.txt https://apkpure.com/de/freestyle-librelink-de/com.freestylelibre.app.de/download/4751-APK
+# disable certificate check
+if [ ! -e ~/.wgetrc ] ; then
+  echo "check_certificate = off" >> ~/.wgetrc
+fi
+
+echo -e "${WHITE}Lade original APK Version 2.3.0 herunter ...${NORMAL}"
+wget -O APK/apkpure.html --keep-session-cookies --save-cookies cookies.txt -U chrome https://apkpure.com/de/freestyle-librelink-de/com.freestylelibre.app.de/download/4751-APK
 URL=$(grep "hier klicken" APK/apkpure.html | sed 's#^.*https://##' | sed 's/">.*//')
-wget -O APK/${FILENAME}.apk --load-cookies cookies.txt https://${URL}
+wget -O APK/${FILENAME_230}.apk --load-cookies cookies.txt -U chrome https://${URL}
 if [ $? = 0 ]; then
   echo -e "${GREEN}  okay.${NORMAL}"
   echo
@@ -29,6 +35,22 @@ else
 fi
 rm cookies.txt
 rm APK/apkpure.html
+
+#echo -e "${WHITE}Lade original APK Version 2.4.0 herunter ...${NORMAL}"
+#wget -O APK/apkpure.html --keep-session-cookies --save-cookies cookies.txt https://apkpure.com/de/freestyle-librelink-de/com.freestylelibre.app.de/download/5417-APK
+#URL=$(grep "hier klicken" APK/apkpure.html | sed 's#^.*https://##' | sed 's/">.*//')
+#wget -O APK/${FILENAME_240}.apk --load-cookies cookies.txt https://${URL}
+#if [ $? = 0 ]; then
+#  echo -e "${GREEN}  okay.${NORMAL}"
+#  echo
+#else
+#  echo -e "${RED}  nicht okay.${NORMAL}"
+#  echo
+#  echo -e "${YELLOW}=> Bitte prüfen Sie o.a. Fehler.${NORMAL}"
+#  exit 1
+#fi
+#rm cookies.txt
+#rm APK/apkpure.html
 
 echo -e "${WHITE}Lade 'apktool' herunter ...${NORMAL}"
 echo "Info: Debian liefert eine nicht ohne weiteres funktionierende 'dirty'-Version mit. Daher der externe Download."
